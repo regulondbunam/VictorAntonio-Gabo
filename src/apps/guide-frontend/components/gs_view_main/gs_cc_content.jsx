60089f9e99b285cef969dfa11006300b7fd36451
victorbalbuena <balbuenabellovictorantonio@gmail.com>
2021-04-30 23:33:28 -0500

    Se agregaron estilos para gs_cc_content

--- src/apps/guide-frontend/components/gs_view_main/gs_cc_content.jsx
+++ src/apps/guide-frontend/components/gs_view_main/gs_cc_content.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import conf from '../../conf/gs_view_main.conf.json'
 
+import styles from './gs_cc_content.module.css';
+
 const description = conf.gs_cc_buttons.description;
 
 const GsCcContent = () => {
   return (
-    <p dangerouslySetInnerHTML={
-      {
-        __html:description
-      }
-    }>
-      
-    </p>
+    
+
+      <p 
+        className={styles.parrafos} dangerouslySetInnerHTML={
+          {
+            __html:description
+          }
+      }>        
+      </p>
+    
+    
   )
 }
 
