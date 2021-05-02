2e9acc8b7e256b088497fba09eaa03d560b4d512
victorbalbuena <balbuenabellovictorantonio@gmail.com>
2021-05-01 22:50:32 -0500

    Se agrego codigo para componentes GS_UI_component

--- src/apps/guide-frontend/components/gs_ui_component.jsx
+++ src/apps/guide-frontend/components/gs_ui_component.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { Cover as GSccHead } from '../ui-components/ui_components';
+import GsCcContent from './gs_ui_component/gs_cc_content';
 
 export default function GsUiComponent() {
   return (
-    <div>
-      <h3>Hola, funciona</h3>
+    <>
+      <GSccHead>
+        <h1>User Interface Components</h1>
+      </GSccHead>
+
+      <GsCcContent />
       
-    </div>
+    </>
   )
 }
