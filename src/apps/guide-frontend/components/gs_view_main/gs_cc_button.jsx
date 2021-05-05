dd496f7efbddb439727bb67e258c10b9ece2c95a
victorbalbuena <balbuenabellovictorantonio@gmail.com>
2021-05-04 22:43:48 -0500

    Correcciones menores y se agregaron comentarios

--- src/apps/guide-frontend/components/gs_view_main/gs_cc_button.jsx
+++ src/apps/guide-frontend/components/gs_view_main/gs_cc_button.jsx
@@ -3,9 +3,14 @@ import { useHistory } from "react-router-dom";
 
 import styles from './gs_cc_buttons.module.css';
 
-function GsCcButton ({ title, id, link, props, content }) {
+function GsCcButton ({ title, id, link }) {
+  // Uso del hook useHistory para navegar a una ruta
   let history = useHistory();
   function handleClick() {
+    /*
+      Las variables 'link' e 'id'  se reciben como PROPS de 'gs_cc_buttons.js' 
+      y a us vez este los consulta del archivo JSON 'gs_cC_content'
+    */
     history.push(`${link}/${id}`);
   }
   
@@ -17,7 +22,6 @@ function GsCcButton ({ title, id, link, props, content }) {
       id={id}
       className={styles.btn}
       onClick={handleClick}
-      content={content}
     >
       {title}
     </button>
--- src/apps/guide-frontend/components/gs_view_main/gs_cc_buttons.jsx
+++ src/apps/guide-frontend/components/gs_view_main/gs_cc_buttons.jsx
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react'
+// Importación del componente gs_cc_button
 import GsCcButton from './gs_cc_button';
+// Importación del archivo JSON que contiene la información de los botones
 import conf from '../../conf/gs_view_main.conf.json'
+// Extracción de los estilos para los botones
 import styles from './gs_cc_buttons.module.css';
 
 const buttons = conf.gs_cc_buttons.buttons
@@ -9,13 +12,12 @@ function GsCcButtons() {
   return (
     <div className={styles.container}>
       {
+        // Se recorre el array proveniente del archivo JSON y se convierte en un componente tiipo gs_cc_button
         buttons.map(button => (
           <GsCcButton 
             key={`button_main_${button.id}_${button.title}`} 
             title={button.title} 
-            link={button.link} 
-            props={button.props}
-            content={button.content} 
+            link={button.link}
             id={button.id}
             />
         ))
