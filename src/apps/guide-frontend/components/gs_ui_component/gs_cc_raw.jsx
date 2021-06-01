2cde3bb015d456507b6aa9672d8e3765cb25fd97
victorbalbuena <balbuenabellovictorantonio@gmail.com>
2021-05-31 23:37:32 -0500

    Filtrado de encabezado en GsCcRaw para UiComponent

--- src/apps/guide-frontend/components/gs_ui_component/gs_cc_raw.jsx
+++ src/apps/guide-frontend/components/gs_ui_component/gs_cc_raw.jsx
@@ -3,19 +3,33 @@ import GsCcMarkdown from './gs_cc_markdown';
 
 const GSCcRaw = () => {
 
-  const [ data, setData ] = useState();
+  const [ data, setData ] = useState("");
 
   useEffect(()=>{
     fetch('https://raw.githubusercontent.com/regulondbunam/Component-Repository/ui-components/ui-components/input/buttons/button.jsx')
       .then(response => response.text())
       .then(text => setData(text))
-  })
+  }, [data])
+
+  const inicio = `/**`;
+  const final = `**/`;
+
+  // let Ninicio = data.indexOf(inicio);
+  // let Nfinal = data.indexOf(final)
+
+  // console.log(Ninicio, Nfinal)
+
+  let cleanData = data.substring(3, data.indexOf(final));
+
 
   return (
     <div>
-      <h2>Funciona</h2>
-      
-        <GsCcMarkdown txt={data}/>
+    <h1></h1>
+      {
+        data==="" ? 'Cargando...' :
+          <GsCcMarkdown txt={cleanData}/>
+
+      }
     </div>
   )
 }
